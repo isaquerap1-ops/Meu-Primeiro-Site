cd75339eb336f02235b84a2f8462b7e40e08ba15
isaquerap1-ops <isaquerap1@gmail.com>
2026-08-11 20:39:00 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Mensagem de teste para verificar se o JS está funcionando
+console.log("Arquivo JavaScript conectado com sucesso!");
+// Exemplo: Alerta de boas-vindas ao carregar a página
+window.onload = function() {
+alert("Bem-vindo ao portfólio de SEU NOME!");
+};
